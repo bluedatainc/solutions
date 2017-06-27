fd1262344fe972e871577a46521bcbe1f2a28b86
Sahithi Gunna <sgunna@bluedata.com>
2017-06-27 11:36:50 -0700

    updated scripts

---
+++ ActionScripts/scripts/Install_python_libraries.sh
@@ -0,0 +1,2 @@
+ sudo yum -y install epel-release &&
+ sudo yum -y install gcc gcc-c++ python-pip python-devel atlas atlas-devel gcc-gfortran openssl-devel libffi-develi;
---
+++ ActionScripts/scripts/python_example_job.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env /opt/anaconda3/bin/python
+  import numpy as np
+  import sys
+  
+  a = np.array([1, 2, 3])  # Create a rank 1 array
+  print (type(a))            # Prints ""
+  print (a.shape)            # Prints "(3,)"
+  print (a[0], a[1], a[2])   # Prints "1 2 3"
+  a[0] = 5                 # Change an element of the array
+  print  (a)                  # Prints "[5, 2, 3]"
+  b = np.array([[1,2,3],[4,5,6]])   # Create a rank 2 array
+  print (b.shape)                     # Prints "(2, 3)"
+  print (b[0, 0], b[0, 1], b[1, 0])   # Prints "1 2 4"
