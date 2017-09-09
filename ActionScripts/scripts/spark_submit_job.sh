51b3ba6ad391a8e2df288db6418c5b9a7f270b26
root <root@yav-203.lab.bluedata.com>
2017-09-08 17:43:01 -0700

    Added a new branch for Image samples
    Signed-off-by: root <root@yav-203.lab.bluedata.com>

--- ActionScripts/scripts/spark_submit_job.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-export node=`bdvcli --get node.role_id`
-if [[ $node == "controller" ]]; then
-    /usr/lib/spark/spark-2.1.1-bin-hadoop2.7/bin/spark-submit --class org.apache.spark.examples.SparkPi --master spark://bluedata-4.bdlocal:7077 /usr/lib/spark/spark-2.1.1-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.1.1.jar 100
-fi
