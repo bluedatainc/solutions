f2e6276d33fa44dafaf767b12c5ce9212ad019f2
root <root@yav-029.lab.bluedata.com>
2019-08-28 16:34:10 -0700

    delete stuff

--- ActionScripts/scripts/Jupyter_restart.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
- export node=`bdvcli --get node.role_id`
- if [[ $node == "controller" ]]; then
- sudo service jupyter-server stop && sudo service jupyter-server start &
- fi
-
- sudo service jupyter-server status &
--- ActionScripts/scripts/Spark_on_Mesos_job.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
- export node=`bdmacro node --get_self_index`
- if [[ $node == 0 ]]; then
- /usr/lib/spark/spark-2.1.1-bin-hadoop2.6/bin/spark-submit --class org.apache.spark.examples.SparkPi --master mesos://zk://10.39.250.6:2181,10.39.250.7:2181,10.39.250.10:2181/mesos /usr/lib/spark/spark-2.1.1-bin-hadoop2.6/examples/jars/spark-examples_2.11-2.1.1.jar 100
- fi
