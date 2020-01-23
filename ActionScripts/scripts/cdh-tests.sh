66a2dfe647c5c4b666e4756506639892fced6312
Kartik <kartik@bluedata.com>
2020-01-23 14:36:01 -0800

    Update cdh-tests.sh

--- ActionScripts/scripts/cdh-tests.sh
+++ ActionScripts/scripts/cdh-tests.sh
@@ -2,7 +2,7 @@ echo "Starting testing jobs"
 echo "generating data using teragen on tenant storage..."
 echo "cleaning up dtap://TenantStorage/tmp from tenant storage if exists"
 sudo -E -u hdfs hadoop fs -rm -r dtap://TenantStorage/tmp
-sudo -E -u hdfs yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen 10000 dtap://TenantStorage/tmp/teragen
+sudo -E -u hdfs yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen 1000000 dtap://TenantStorage/tmp/teragen
 sudo -E -u hdfs yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar wordcount dtap://TenantStorage/tmp/teragen/part-m-00000 dtap://TenantStorage/tmp/out
 echo "Successfully ran teragen and wordcount on tenant storage"
 echo "Test completed"
