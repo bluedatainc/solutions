a2b73ea0e1b58facf971d5614d12dc927f6774f6
saurabh-jogalekar <saurabh.jogalekar@hpe.com>
2020-03-24 15:27:50 -0700

    Sample pipelines for kubeflow
    mnist_train:
    
     Dockerfile for creating the image and model file to deploy the image with the DOckerfile
    
    mnist_test:
    
    Dockerfile to create the image and model file to load the model and calcuate the accuracy metrics
    
    Note: The Dockerfiles are not enabled with proxy. As a result, they shall not be able to download. Please set the empty proxy variables inside the dockerfiles to enable proxy.

---
+++ AI-ML-SampleCode/kubeflow/mnist_test/three_step_test.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+import tensorflow as tf
+import numpy as np
+import os
+import sys
+
+model_file = str(sys.argv[1])
+data_file = str(sys.argv[2])
+
+#model = tf.keras.models.Sequential([
+#    tf.keras.layers.Flatten(input_shape=(28, 28)),
+#    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+#    tf.keras.layers.Dropout(0.2),
+#    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+#])
+
+#model.compile(optimizer='adam',
+#                loss='sparse_categorical_crossentropy',
+#                metrics=['accuracy'])
+
+#print(model.summary())    
+#result = os.popen("curl  -O http://16.143.20.46:8080/scratch/saurabh/mnist.npz").read()
+arr =  np.load(data_file)
+X_train = arr['x_train']
+y_train = arr['y_train']
+X_test = arr['x_test']
+y_test = arr['y_test']
+#(X_train, y_train), (X_test, y_test)= tf.keras.datasets.mnist.load_data()
+X_train, X_test = X_train / 255.0, X_test / 255.0
+
+#     callbacks = [
+#       tf.keras.callbacks.TensorBoard(log_dir='/home/joyvan' + '/logs/' + datetime.now().date().__str__()),
+#       # Interrupt training if `val_loss` stops improving for over 2 epochs
+#       tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+#     ]
+
+#model.fit(X_train, y_train, batch_size=32, epochs=5,
+#            validation_data=(X_test, y_test))
+
+#print("Done with fitting")
+#model.save(model_file)
+
+print("Loading model from : " + model_file)
+
+model = tf.keras.models.load_model(model_file, compile=True)
+print("Model result is")
+print(model.evaluate(x=X_test,y=y_test))
